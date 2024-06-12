fea0b73f95d86538beda6e1e3898566687508b66
Mikalai Holias <40669618+lighter952@users.noreply.github.com>
2024-06-12 18:34:43 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask import abort, redirect, url_for, make_response
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return "Hello World!"
+def index():
+ return redirect("/about")
+
+
+@app.route('/about')
+def about():
+ return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+ return render_template("contact.html")
+
+
+@app.route('/gallery')
+def gallery():
+ return render_template("gallery.html")
+
+
+@app.route('/error_denied')
+def error_denied():
+ abort(401)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+ app.run(debug=False, port=8000)
